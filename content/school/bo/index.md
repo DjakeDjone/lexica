4926fbd50e2210442f4623533d65119bfe1ed712
DjakeDjone <103126219+DjakeDjone@users.noreply.github.com>
2024-03-25 10:20:27 +0100

    setup

---
+++ content/school/bo/index.md
@@ -0,0 +1,4 @@
+# BO
+
+## Table of contents
+
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxt/content'],
+  content: {
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'github-light',
+        // Theme used if `html.dark`
+        dark: 'github-dark',
+        // Theme used if `html.sepia`
+        sepia: 'monokai'
+      }
+    }
+  }
+})
