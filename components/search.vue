bd41e8cbe4ce16dc2fcd52f6ef701fa99a8ff68f
DjakeDjone <benjamin.friedl@htlstp.at>
2024-05-11 15:31:33 +0200

    advanced Search function with key support

--- components/search.vue
+++ components/search.vue
@@ -5,6 +5,7 @@ import type { NavItem } from '~/app.vue';
 const searchVlue = ref('');
 
 const value = defineModel();
+const selected = ref(0);
 
 
 const emit = defineEmits(['update:close']);
@@ -53,7 +54,30 @@ const search = async (value: string) => {
 
 let searchTimeout: NodeJS.Timeout | null = null;
 
-const searchOnTypeEnd = async () => {
+const searchOnTypeEnd = async (event: KeyboardEvent | FocusEvent) => {
+    console.log(event);
+    
+    if (event instanceof KeyboardEvent && event.key === 'ArrowUp') {
+        if (selected.value > 0) {
+            selected.value--;
+        }
+        return;
+    } else if (event instanceof KeyboardEvent && event.key === 'ArrowDown') {
+        if (selected.value < searchResults.value.length - 1) {
+            selected.value++;
+        }
+        return;
+    } else if (event instanceof KeyboardEvent && event.key === 'Enter') {
+        if (searchResults.value.length > 0) {
+            const result = searchResults.value[selected.value];
+            if (result) {
+                await useRouter().push(result._path);
+                searchVlue.value = '';
+                close();
+            }
+        }
+        return;
+    }
     if (searchTimeout) {
         clearTimeout(searchTimeout);
     }
@@ -85,15 +109,17 @@ const close = () => {
             <div class="w-full flex items-start gap-2">
                 <textarea class="textarea resize-none w-full h-8" v-model="searchVlue"
                     placeholder="Search..." @keydown.enter="searchEvent($event)"
-                    @keyup="searchOnTypeEnd()" @focus="searchOnTypeEnd()" @click="searchOnTypeEnd()"
+                    @keyup="searchOnTypeEnd($event)" @focus="searchOnTypeEnd($event)" @click="searchOnTypeEnd($event)"
                     ></textarea>
                 <button class="btn btn-primary" @click="searchEvent($event)">
                     <Icon name="line-md:search" size="24" />
                 </button>
             </div>
             <div v-auto-animate id="search-results" class="mt-2 bg-base-100 rounded-lg">
-                <!-- {{ searchResults }} -->
-                <div v-for="result in searchResults" :key="result._path" class="searchResult rounded-xl border border-transparent p-3 pb-0 hover:bg-base-100/50">
+                <!-- {{ selected }} -->
+                <div v-for="result, idx in searchResults" :key="result._path" class="searchResult rounded-xl border border-transparent p-3 pb-0 hover:bg-base-100/50"
+                    :class="{ 'border-primary': idx === selected }"
+                >
                     <router-link :to="result._path" @click="searchVlue = '';close()"
                         class="m-0 hover:no-underline">
                         <h3 class="text-lg font-bold mt-0">{{ result.title }}</h3>
